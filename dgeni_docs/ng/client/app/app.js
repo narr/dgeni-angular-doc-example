0a34df185bc001a6064bd8ac65d22aa59a15dd12
Narr <nardgu@gmail.com>
2015-08-02 21:23:10 +0900

    Organize sources

--- dgeni_docs/ng/client/app/app.js
+++ dgeni_docs/ng/client/app/app.js
@@ -1,22 +1,13 @@
 /*!
- * Doc App
+ * Doc App(referred to https://github.com/angular/angular.js/tree/master/docs)
  */
 (function(angular) {
-
     'use strict';
 
     angular.module('docApp', ['ui.bootstrap'])
         .constant('DOCS_OVERWRITELINK', angular.element('base').length < 1)
-        //.provider('DOCS_OVERWRITELINK', function(DOCS_OVERWRITELINK) {
-        //    return {
-        //        $get: function() {
-        //            return DOCS_OVERWRITELINK;
-        //        }
-        //    };
-        //})
         .config(function($locationProvider, DOCS_OVERWRITELINK) {
             if (!DOCS_OVERWRITELINK) {
-                $locationProvider.hashPrefix('!');
                 $locationProvider.html5Mode({
                     enabled: true,
                     requireBase: false,
